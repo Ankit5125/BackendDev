deb6b1c95117380f6396a7027140d176907f03bc
Ankit <savaliyaankit5125@gmail.com>
2025-11-05 13:49:00 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require('express');
+const app = express()
+
+app.listen(process.env.PORT, ()=> {
+    console.log(`✅ Server started at port ${process.env.PORT}`);
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to My Learning Repository!');
+})
