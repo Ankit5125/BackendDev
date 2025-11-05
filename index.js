22950bfd76e086ec36322b3ab112eab803de21a6
Ankit <savaliyaankit5125@gmail.com>
2025-11-05 13:55:03 +0530

    added About Project

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ app.listen(process.env.PORT, ()=> {
 app.get('/', (req, res) => {
     res.send('Welcome to My Learning Repository!');
 })
+
+app.get('/about', (req, res) =>{
+    res.json({
+        status : "success",
+        name : "Learning Repository",
+        version : "1.0.0",
+        author : "Ankit Savaliya"
+    })
+})
