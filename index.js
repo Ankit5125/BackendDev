8ca2e2a2b57e6b09ff8c4fcbe0ab7eea18b54f92
Ankit <savaliyaankit5125@gmail.com>
2025-11-21 23:46:32 +0530

    Connection with MongoDB & adding a User to DB

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-require('dotenv').config()
+const mongoose = require('mongoose');
 const express = require('express');
+const { user } = require('./models/todos/user.model');
 const app = express()
+app.use(express.json());
 
-app.listen(process.env.PORT, ()=> {
+require('dotenv').config()
+
+app.listen(process.env.PORT, async () => {
+    await mongoose.connect(`${process.env.DBURL}`).then((response) =>{
+        console.log("❓Mongoose Connection Status : " + mongoose.connection.readyState)
+    })
     console.log(`✅ Server started at port ${process.env.PORT}`);
 })
 
@@ -10,25 +17,53 @@ app.get('/', (req, res) => {
     res.send('Welcome to My Learning Repository!');
 })
 
-app.get('/about', (req, res) =>{
+app.get('/about', (req, res) => {
     res.json({
-        status : "success",
-        name : "Learning Repository",
-        version : "1.0.0",
-        author : "Ankit Savaliya"
+        status: "success",
+        name: "Learning Repository",
+        version: "1.0.0",
+        author: "Ankit Savaliya"
     })
 })
 
-app.get('/timetable', (req, res) =>{
+app.get('/timetable', (req, res) => {
     res.json({
-        status : "success",
-        timetable : {
-            "14 Nov" : "IPDC",
-            "17 Nov" : "PDS",
-            "19 Nov" : "SE",
-            "21 Nov" : "CN",
-            "25 Nov" : "PE",
-            "2 Dec" : "ADA",
+        status: "success",
+        timetable: {
+            "14 Nov": "IPDC",
+            "17 Nov": "PDS",
+            "19 Nov": "SE",
+            "21 Nov": "CN",
+            "25 Nov": "PE",
+            "2 Dec": "ADA",
         }
     })
+})
+
+app.post("/addUser", async (req, res) => {
+   let { username, password, email, number, isActive } = req.body;
+    const newUser = new user({
+            username: username,
+            email: email,
+            number: number,
+            password: password
+    })
+    console.log({
+        username: username,
+            email: email,
+            number: number,
+            password: password
+    })
+    try {
+        await newUser.save().then(
+            response => {
+                res.send("Status : "+ response)
+            }
+        ).catch(err => {
+            res.send('Error saving document: '+ err);
+        })
+    }
+    catch (err) {
+        res.send('Error Building Connection : '+ err);
+    }
 })
